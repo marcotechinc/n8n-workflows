974a25f532aab72d08662fbbd6ab86bfcf9a7a12
Marco San Juan <marcotechinc@outlook.com>
2026-02-03 19:05:50 -0800

    Sanity test

--- MLOps/streams_worker/index.js
+++ MLOps/streams_worker/index.js
@@ -1,17 +1,5 @@
-import express from "express";
+console.log("BOOTING");
 
-const app = express();
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-  res.send("ok");
-});
-
-app.post("/events", (_req, res) => {
-  res.json({ ok: true });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`streams_worker listening on ${PORT}`);
-});
+setInterval(() => {
+  console.log("ALIVE");
+}, 5000);
